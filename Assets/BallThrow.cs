a0890e6b9207967daec3ff15dc9d3f16b5e1a38c
Damian Wojtowicz <47143782+damianazur@users.noreply.github.com>
2021-04-27 09:57:34 +0100

    Ball throwing implemented

---
+++ Assets/BallThrow.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallThrow : MonoBehaviour
+{
+    private GameObject mainCamera;
+    public GameObject ballPrefab;
+    public float ballVelocity = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (mainCamera == null)
+        {
+            mainCamera = Camera.main.gameObject;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Spawn ball
+            GameObject ball = GameObject.Instantiate<GameObject>(ballPrefab);
+
+            // Set position and rotation to that of the camera
+            ball.transform.position = mainCamera.transform.position;
+            ball.transform.rotation = mainCamera.transform.rotation;
+            Rigidbody ballRigidbody = ball.GetComponent<Rigidbody>();
+
+            // Add force to ball
+            ballRigidbody.AddForce(ball.transform.forward * ballVelocity, ForceMode.Impulse);
+        }
+    }
+}
---
+++ Assets/States.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Active:State
+{
+    public override void Think()
+    {
+
+    }
+}
+
+public class Fetch: State
+{
+    public override void Enter()
+    {
+        
+    }
+
+    public override void Think()
+    {
+        
+    }
+
+    public override void Exit()
+    {
+        
+    }    
+}
+
+public class ReturnToOwner: State
+{
+    public override void Enter()
+    {
+       
+    }
+
+    public override void Think()
+    {
+        
+    }
+
+    public override void Exit()
+    {
+        
+    }
+}
